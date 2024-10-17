38d0a84c60c008c18a4a168154d570324ea28c6e
Abhigyan Sharma <132127909+Abhigyan1112@users.noreply.github.com>
2024-10-17 23:21:17 +0530

    Update DeliveryEmployeeRepository.java

--- src/main/java/com/vastrika/backend/DeliveryEmployee/repository/DeliveryEmployeeRepository.java
+++ src/main/java/com/vastrika/backend/DeliveryEmployee/repository/DeliveryEmployeeRepository.java
@@ -2,9 +2,12 @@ package com.vastrika.backend.DeliveryEmployee.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.vastrika.backend.DeliveryEmployee.model.DeliveryEmployee;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 @Repository
 
 public interface DeliveryEmployeeRepository extends JpaRepository < DeliveryEmployee,String>{
-   //repo
+   @Query(value= "SELECT * FROM DeliveryEmployee where city= :city", nativeQuery=true)
+   List<DeliveryEmployee> getDeliveryEmployeeByCity(@Param("city") String city);
 }
